697b53ed45575cc6bd8a7e999e51ce61dc7671c1
Danglebary <halfblowncontact@gmail.com>
2022-02-05 00:18:32 -0600

    added random funcs for beat tests

--- util/random.go
+++ util/random.go
@@ -8,6 +8,31 @@ import (
 
 const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+var musicalKeys [22]string = [22]string{
+	"C major",
+	"C minor",
+	"C# major",
+	"C# minor",
+	"D major",
+	"D minor",
+	"D# major",
+	"D# minor",
+	"E major",
+	"E minor",
+	"F major",
+	"F minor",
+	"G major",
+	"G minor",
+	"G# major",
+	"G# minor",
+	"A major",
+	"A minor",
+	"A# major",
+	"A# minor",
+	"B major",
+	"B minor",
+}
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
@@ -30,14 +55,48 @@ func RandomString(n int) string {
 	return sb.String()
 }
 
+// Generates a random string of length 6
 func RandomUsername() string {
 	return RandomString(6)
 }
 
+// Generates a random string of length 6
 func RandomPassword() string {
 	return RandomString(6)
 }
 
+// Generates a random string of format 6char + "@" + 6char + ".com"
 func RandomEmail() string {
 	return RandomString(6) + "@" + RandomString(6) + ".com"
+}
+
+// Generates a random string of length 6
+func RandomTitle() string {
+	return RandomString(6)
+}
+
+func RandomGenre() string {
+	return RandomString(6)
+}
+
+func RandomKey() string {
+	k := len(musicalKeys)
+	return musicalKeys[rand.Intn(k)]
+}
+
+func RandomBpm() int16 {
+	return int16(RandomInt(20, 999))
+}
+
+func RandomTags() string {
+	str := ""
+
+	for i := 0; i < int(RandomInt(0, 5)); i++ {
+		str += RandomString(int(RandomInt(3, 8))) + ","
+	}
+	return str
+}
+
+func RandomS3Key() string {
+	return RandomString(12)
 }
