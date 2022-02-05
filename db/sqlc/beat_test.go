1cacf5a88bb87a9a050659f7e3ec0daf5f362f51
Danglebary <halfblowncontact@gmail.com>
2022-02-05 01:56:20 -0600

    created tests for CRUD ops on beats table

--- db/sqlc/beat_test.go
+++ db/sqlc/beat_test.go
@@ -1,1 +1,430 @@
 package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/danglebary/beatstore-backend/util"
+	"github.com/stretchr/testify/require"
+)
+
+func CreateRandomBeat(t *testing.T) Beat {
+	user1 := CreateRandomUser(t)
+
+	arg := CreateBeatParams{
+		CreatorID: user1.ID,
+		Title:     util.RandomTitle(),
+		Genre:     util.RandomGenre(),
+		Key:       util.RandomKey(),
+		Bpm:       util.RandomBpm(),
+		Tags:      util.RandomTags(),
+		S3Key:     util.RandomS3Key(),
+	}
+
+	beat, err := testQueries.CreateBeat(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beat)
+
+	require.Equal(t, user1.ID, beat.CreatorID)
+	require.Equal(t, arg.Title, beat.Title)
+	require.Equal(t, arg.Genre, beat.Genre)
+	require.Equal(t, arg.Key, beat.Key)
+	require.Equal(t, arg.Bpm, beat.Bpm)
+	require.Equal(t, arg.Tags, beat.Tags)
+	require.Equal(t, arg.S3Key, beat.S3Key)
+
+	require.NotZero(t, beat.ID)
+	require.NotZero(t, beat.CreatedAt)
+
+	return beat
+}
+
+func createRandomBeatWithArgs(t *testing.T, arg CreateBeatParams) Beat {
+	beat, err := testQueries.CreateBeat(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beat)
+
+	require.Equal(t, arg.CreatorID, beat.CreatorID)
+	require.Equal(t, arg.Title, beat.Title)
+	require.Equal(t, arg.Genre, beat.Genre)
+	require.Equal(t, arg.Key, beat.Key)
+	require.Equal(t, arg.Bpm, beat.Bpm)
+	require.Equal(t, arg.Tags, beat.Tags)
+	require.Equal(t, arg.S3Key, beat.S3Key)
+
+	require.NotZero(t, beat.ID)
+	require.NotZero(t, beat.CreatedAt)
+
+	return beat
+}
+
+func DeleteRandomBeat(t *testing.T, id int32) {
+	err := testQueries.DeleteBeat(context.Background(), id)
+	require.NoError(t, err)
+}
+
+func TestCreateBeat(t *testing.T) {
+	beat1 := CreateRandomBeat(t)
+
+	DeleteRandomBeat(t, beat1.ID)
+	DeleteRandomUser(t, beat1.CreatorID)
+}
+
+func TestUpdateBeat(t *testing.T) {
+	beat1 := CreateRandomBeat(t)
+
+	arg := UpdateBeatParams{
+		ID:         beat1.ID,
+		Title:      util.RandomTitle(),
+		Genre:      util.RandomGenre(),
+		Key:        util.RandomKey(),
+		Bpm:        util.RandomBpm(),
+		Tags:       util.RandomTags(),
+		S3Key:      util.RandomS3Key(),
+		LikesCount: util.RandomLikesCount(),
+	}
+
+	beat2, err := testQueries.UpdateBeat(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beat2)
+
+	require.Equal(t, beat1.ID, beat2.ID)
+	require.Equal(t, arg.Title, beat2.Title)
+	require.Equal(t, arg.Genre, beat2.Genre)
+	require.Equal(t, arg.Key, beat2.Key)
+	require.Equal(t, arg.Bpm, beat2.Bpm)
+	require.Equal(t, arg.Tags, beat2.Tags)
+	require.Equal(t, arg.S3Key, beat2.S3Key)
+	require.Equal(t, arg.LikesCount, beat2.LikesCount)
+	require.WithinDuration(t, beat1.CreatedAt, beat2.CreatedAt, time.Second)
+
+	DeleteRandomBeat(t, beat1.ID)
+	DeleteRandomUser(t, beat1.CreatorID)
+}
+
+func TestGetBeatById(t *testing.T) {
+	beat1 := CreateRandomBeat(t)
+
+	beat2, err := testQueries.GetBeatById(context.Background(), beat1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, beat2)
+
+	require.Equal(t, beat1.ID, beat2.ID)
+	require.Equal(t, beat1.CreatorID, beat2.CreatorID)
+	require.Equal(t, beat1.Title, beat2.Title)
+	require.Equal(t, beat1.Genre, beat2.Genre)
+	require.Equal(t, beat1.Key, beat2.Key)
+	require.Equal(t, beat1.Bpm, beat2.Bpm)
+	require.Equal(t, beat1.LikesCount, beat2.LikesCount)
+	require.Equal(t, beat1.S3Key, beat2.S3Key)
+	require.WithinDuration(t, beat1.CreatedAt, beat2.CreatedAt, time.Second)
+
+	DeleteRandomBeat(t, beat1.ID)
+	DeleteRandomUser(t, beat1.CreatorID)
+}
+
+func TestListBeatsById(t *testing.T) {
+	n := 10
+	for i := 0; i < n; i++ {
+		CreateRandomBeat(t)
+	}
+	arg := ListBeatsByIdParams{
+		Limit:  int32(n),
+		Offset: 0,
+	}
+	beats, err := testQueries.ListBeatsById(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beats)
+	require.Equal(t, n, len(beats))
+
+	for _, beat := range beats {
+		require.NotEmpty(t, beat)
+		DeleteRandomBeat(t, beat.ID)
+		DeleteRandomUser(t, beat.CreatorID)
+	}
+}
+
+func TestListBeatsByCreatorId(t *testing.T) {
+	user1 := CreateRandomUser(t)
+
+	beatArg := CreateBeatParams{
+		CreatorID: user1.ID,
+		Title:     util.RandomTitle(),
+		Genre:     util.RandomGenre(),
+		Key:       util.RandomKey(),
+		Bpm:       util.RandomBpm(),
+		Tags:      util.RandomTags(),
+		S3Key:     util.RandomS3Key(),
+	}
+
+	n := 10
+	for i := 0; i < n; i++ {
+		createRandomBeatWithArgs(t, beatArg)
+	}
+
+	arg := ListBeatsByCreatorIdParams{
+		CreatorID: user1.ID,
+		Limit:     int32(n),
+		Offset:    0,
+	}
+	beats, err := testQueries.ListBeatsByCreatorId(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beats)
+	require.Equal(t, n, len(beats))
+
+	for _, beat := range beats {
+		require.NotEmpty(t, beat)
+		DeleteRandomBeat(t, beat.ID)
+	}
+
+	DeleteRandomUser(t, user1.ID)
+}
+
+func TestListBeatsbyGenre(t *testing.T) {
+	genre := util.RandomGenre()
+
+	n := 10
+	for i := 0; i < n; i++ {
+		user := CreateRandomUser(t)
+		args := CreateBeatParams{
+			CreatorID:  user.ID,
+			Title:      util.RandomTitle(),
+			Genre:      genre,
+			Key:        util.RandomKey(),
+			Bpm:        util.RandomBpm(),
+			Tags:       util.RandomTags(),
+			S3Key:      util.RandomS3Key(),
+			LikesCount: util.RandomLikesCount(),
+		}
+		createRandomBeatWithArgs(t, args)
+	}
+
+	arg := ListBeatsByGenreParams{
+		Genre:  genre,
+		Limit:  int32(n),
+		Offset: 0,
+	}
+	beats, err := testQueries.ListBeatsByGenre(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beats)
+	require.Equal(t, n, len(beats))
+
+	for _, beat := range beats {
+		require.NotEmpty(t, beat)
+		require.Equal(t, genre, beat.Genre)
+		DeleteRandomBeat(t, beat.ID)
+		DeleteRandomUser(t, beat.CreatorID)
+	}
+}
+
+func TestListBeatsByKey(t *testing.T) {
+	key := util.RandomKey()
+
+	n := 10
+	for i := 0; i < n; i++ {
+		user := CreateRandomUser(t)
+		args := CreateBeatParams{
+			CreatorID:  user.ID,
+			Title:      util.RandomTitle(),
+			Genre:      util.RandomGenre(),
+			Key:        key,
+			Bpm:        util.RandomBpm(),
+			Tags:       util.RandomTags(),
+			S3Key:      util.RandomS3Key(),
+			LikesCount: util.RandomLikesCount(),
+		}
+		createRandomBeatWithArgs(t, args)
+	}
+
+	arg := ListBeatsByKeyParams{
+		Key:    key,
+		Limit:  int32(n),
+		Offset: 0,
+	}
+	beats, err := testQueries.ListBeatsByKey(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beats)
+	require.Equal(t, n, len(beats))
+
+	for _, beat := range beats {
+		require.NotEmpty(t, beat)
+		require.Equal(t, key, beat.Key)
+		DeleteRandomBeat(t, beat.ID)
+		DeleteRandomUser(t, beat.CreatorID)
+	}
+}
+
+func TestListBeatsByBpmRange(t *testing.T) {
+	min := 100
+	max := 200
+
+	n := 10
+	for i := 0; i < n; i++ {
+		user := CreateRandomUser(t)
+		args := CreateBeatParams{
+			CreatorID:  user.ID,
+			Title:      util.RandomTitle(),
+			Genre:      util.RandomGenre(),
+			Key:        util.RandomKey(),
+			Bpm:        int16(util.RandomInt(int64(min), int64(max))),
+			Tags:       util.RandomTags(),
+			S3Key:      util.RandomS3Key(),
+			LikesCount: util.RandomLikesCount(),
+		}
+		createRandomBeatWithArgs(t, args)
+	}
+
+	arg := ListBeatsByBpmRangeParams{
+		Bpm:    int16(min),
+		Bpm_2:  int16(max),
+		Limit:  int32(n),
+		Offset: 0,
+	}
+	beats, err := testQueries.ListBeatsByBpmRange(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beats)
+	require.Equal(t, n, len(beats))
+
+	for _, beat := range beats {
+		require.NotEmpty(t, beat)
+		require.GreaterOrEqual(t, beat.Bpm, int16(min))
+		require.LessOrEqual(t, beat.Bpm, int16(max))
+		DeleteRandomBeat(t, beat.ID)
+		DeleteRandomUser(t, beat.CreatorID)
+	}
+}
+
+func TestListBeatsByCreatorIdAndGenre(t *testing.T) {
+	user1 := CreateRandomUser(t)
+	genre := util.RandomGenre()
+
+	n := 10
+	for i := 0; i < n; i++ {
+		args := CreateBeatParams{
+			CreatorID:  user1.ID,
+			Title:      util.RandomTitle(),
+			Genre:      genre,
+			Key:        util.RandomKey(),
+			Bpm:        util.RandomBpm(),
+			Tags:       util.RandomTags(),
+			S3Key:      util.RandomS3Key(),
+			LikesCount: util.RandomLikesCount(),
+		}
+		createRandomBeatWithArgs(t, args)
+	}
+
+	arg := ListBeatsByCreatorIdAndGenreParams{
+		CreatorID: user1.ID,
+		Genre:     genre,
+		Limit:     int32(n),
+		Offset:    0,
+	}
+	beats, err := testQueries.ListBeatsByCreatorIdAndGenre(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beats)
+	require.Equal(t, n, len(beats))
+
+	for _, beat := range beats {
+		require.NotEmpty(t, beat)
+		require.Equal(t, user1.ID, beat.CreatorID)
+		require.Equal(t, genre, beat.Genre)
+		DeleteRandomBeat(t, beat.ID)
+	}
+	DeleteRandomUser(t, user1.ID)
+}
+
+func TestListBeatsByCreatorIdAndKey(t *testing.T) {
+	user1 := CreateRandomUser(t)
+	key := util.RandomKey()
+
+	n := 10
+	for i := 0; i < n; i++ {
+		args := CreateBeatParams{
+			CreatorID:  user1.ID,
+			Title:      util.RandomTitle(),
+			Genre:      util.RandomGenre(),
+			Key:        key,
+			Bpm:        util.RandomBpm(),
+			Tags:       util.RandomTags(),
+			S3Key:      util.RandomS3Key(),
+			LikesCount: util.RandomLikesCount(),
+		}
+		createRandomBeatWithArgs(t, args)
+	}
+
+	arg := ListBeatsByCreatorIdAndKeyParams{
+		CreatorID: user1.ID,
+		Key:       key,
+		Limit:     int32(n),
+		Offset:    0,
+	}
+	beats, err := testQueries.ListBeatsByCreatorIdAndKey(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beats)
+	require.Equal(t, n, len(beats))
+
+	for _, beat := range beats {
+		require.NotEmpty(t, beat)
+		require.Equal(t, user1.ID, beat.CreatorID)
+		require.Equal(t, key, beat.Key)
+		DeleteRandomBeat(t, beat.ID)
+	}
+	DeleteRandomUser(t, user1.ID)
+}
+
+func TestListBeatsByCreatorIdAndBpmRange(t *testing.T) {
+	user1 := CreateRandomUser(t)
+	min := 100
+	max := 200
+
+	n := 10
+	for i := 0; i < n; i++ {
+		args := CreateBeatParams{
+			CreatorID:  user1.ID,
+			Title:      util.RandomTitle(),
+			Genre:      util.RandomGenre(),
+			Key:        util.RandomKey(),
+			Bpm:        int16(util.RandomInt(int64(min), int64(max))),
+			Tags:       util.RandomTags(),
+			S3Key:      util.RandomS3Key(),
+			LikesCount: util.RandomLikesCount(),
+		}
+		createRandomBeatWithArgs(t, args)
+	}
+
+	arg := ListBeatsByCreatorIdAndBpmRangeParams{
+		CreatorID: user1.ID,
+		Bpm:       int16(min),
+		Bpm_2:     int16(max),
+		Limit:     int32(n),
+		Offset:    0,
+	}
+	beats, err := testQueries.ListBeatsByCreatorIdAndBpmRange(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, beats)
+	require.Equal(t, n, len(beats))
+
+	for _, beat := range beats {
+		require.NotEmpty(t, beat)
+		require.Equal(t, user1.ID, beat.CreatorID)
+		require.GreaterOrEqual(t, beat.Bpm, int16(min))
+		require.LessOrEqual(t, beat.Bpm, int16(max))
+		DeleteRandomBeat(t, beat.ID)
+	}
+	DeleteRandomUser(t, user1.ID)
+}
+
+func TestDeleteBeat(t *testing.T) {
+	beat1 := CreateRandomBeat(t)
+	err := testQueries.DeleteBeat(context.Background(), beat1.ID)
+	require.NoError(t, err)
+
+	beat2, err := testQueries.GetBeatById(context.Background(), beat1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, beat2)
+
+	DeleteRandomUser(t, beat1.CreatorID)
+}
