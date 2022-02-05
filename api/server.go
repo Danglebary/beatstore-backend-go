bfde47176dba4db2d926066692d243eb50a9fd16
Danglebary <halfblowncontact@gmail.com>
2022-02-05 15:35:53 -0600

    created api package for http REST api

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	db "github.com/danglebary/beatstore-backend-go/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Serves all HTTP requests for our service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// Creates a new HTTP server instance and initializes routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/users", server.createUser)
+
+	server.router = router
+	return server
+}
+
+// Runs HTTP server on specified address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
---
+++ api/user.go
@@ -0,0 +1,35 @@
+package api
+
+import (
+	"net/http"
+
+	db "github.com/danglebary/beatstore-backend-go/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type CreateUserRequest struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email"    binding:"required"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req CreateUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username: req.Username,
+		Password: req.Password,
+		Email:    req.Email,
+	}
+
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
+}
