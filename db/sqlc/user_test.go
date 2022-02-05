d6986a39bd82a907e5e87ea74ec4e97219cb281f
Danglebary <halfblowncontact@gmail.com>
2022-02-04 21:35:47 -0600

    added CreateUser query test

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -1,1 +1,27 @@
 package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateUser(t *testing.T) {
+	arg := CreateUserParams{
+		Username: "danglebary",
+		Password: "abc123",
+		Email:    "danglebary@danglebary.com",
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.Password, user.Password)
+	require.Equal(t, arg.Email, user.Email)
+
+	require.NotZero(t, user.ID)
+	require.NotZero(t, user.CreatedAt)
+}
