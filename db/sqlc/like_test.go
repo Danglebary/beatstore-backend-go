48fee7193bfb30c55547fe9dd5e8c7cf69bf4911
Danglebary <halfblowncontact@gmail.com>
2022-02-05 01:56:52 -0600

    created file for testing likes table CRUD ops

--- db/sqlc/like_test.go
+++ db/sqlc/like_test.go
@@ -1,1 +1,19 @@
 package db
+
+import "testing"
+
+func TestCreateLike(t *testing.T) {
+
+}
+
+func TestListLikesByUserId(t *testing.T) {
+
+}
+
+func TestListLikesByBeatId(t *testing.T) {
+
+}
+
+func TestDeleteLike(t *testing.T) {
+
+}
