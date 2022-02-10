744cf8ee75785f9af7fe1fdf5df5f099aad8cfa7
Danglebary <halfblowncontact@gmail.com>
2022-02-10 13:38:07 -0600

    updated store to use postgres or mockdb

--- api/server.go
+++ api/server.go
@@ -7,12 +7,12 @@ import (
 
 // Serves all HTTP requests for our service
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 // Creates a new HTTP server instance and initializes routing
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,21 +6,26 @@ import (
 	"fmt"
 )
 
-// Provides all functions to execute db queries and transactions
-type Store struct {
+// Store provides all functions to execute queries and transactions
+type Store interface {
+	Querier
+}
+
+// SQLStore provides all functions to execute SQL queries and transactions
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
 // executes a function within a db transaction
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, &sql.TxOptions{})
 	if err != nil {
 		return err
